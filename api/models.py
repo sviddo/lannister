fc1529baa7b7d23709156f277f9c727e0e80548c
rashboldb <b.dhzus@yahoo.com>
2022-07-09 15:18:36 +0300

    Add Request model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from pydoc import describe
 from django.core.exceptions import ValidationError
 from django.db import models
 
@@ -21,4 +23,21 @@ class UserRole(models.Model):
                                  role=self.role).exists():
             raise ValidationError("Model already exists")
         super(UserRole, self).validate_unique(exclude)
+
+class Request(models.Model):
+    class Status(models.TextChoices):
+        CREATED = 'c'
+        APPROVED = 'a'
+        REJECTED = 'r'
+        DONE = 'd'
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='request_creator')
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='request_reviewer')
+    status = models.CharField(max_length=1, choices=Status.choices)
+    # ask the user to describe the bonus type in a few words i.e. - referral bonus, overtime, etc.
+    bonus_type = models.CharField(max_length=80)
+    description = models.TextField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+    last_modification_time = models.DateTimeField(auto_now=True)
+    paymant_day = models.DateField(null=True)
 
--- slack/views.py
+++ slack/views.py
@@ -1,11 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from slack_bolt import App
 from slack_bolt.adapter.django import SlackRequestHandler
-import requests, json
+import requests, json, os
 
 app = App(
     #get tokens in the app, do not push to github!!!
-
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
 handler = SlackRequestHandler(app=app)
