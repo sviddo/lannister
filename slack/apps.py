487bb827384c5b31b52b33576625a0e2623ac761
sviddo <snisarvlad04@gmail.com>
2022-07-14 02:35:06 +0300

    basic setup to run the application
    - define global routes (includind '/slack/events' one to manage slack events)
    - allow localhost to run the application
    - automatically create role model objects

--- slack/apps.py
+++ slack/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+import requests
 
 class SlackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'slack'
+
+    def ready(self) -> None:
+        from api.models import Role
+
+        Role(name='cw').save()
+        Role(name='r').save()
+        Role(name='a').save()
