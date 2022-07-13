6309affda2912182a4f3aac8518c946b0a9407ce
sviddo <snisarvlad04@gmail.com>
2022-07-14 02:41:57 +0300

    create '/api/users' endpoint to get all users and view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import (get_users)
+
 
 urlpatterns = [
-    path('/users'),
+    path('users', get_users, name='get-users'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import (
+    User, 
+    UserRole, 
+)
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def get_users(requests):
+    users = User.objects.all()
+    if users:
+        users_with_roles = []
+        for user in users:
+            service_id = user.service_id
+            roles = []
+            for user_role in UserRole.objects.filter(user=service_id):
+                roles.append(user_role.role.name)
+
+            users_with_roles.append({'service_id': service_id, 'roles': roles})
+        return Response(users_with_roles, status=status.HTTP_200_OK)
+    else:
+        return Response(["No users!"], status=status.HTTP_404_NOT_FOUND)
