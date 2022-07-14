76383b4f169fb4ddc0c5a7ed7ee1776fc0c9f93d
sviddo <snisarvlad04@gmail.com>
2022-07-14 12:56:22 +0300

    add auto-registering of role model objects

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import Role
 
-admin.site.register(Role)
+# Register your models here.
--- slack/apps.py
+++ slack/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
-import requests
+from django.db.utils import OperationalError
 
 class SlackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'slack'
+
+    def ready(self) -> None:
+        from api.models import Role
+
+        try:
+            Role(name='cw').save()
+            Role(name='r').save()
+            Role(name='a').save()
+        except OperationalError:
+            pass
+        
+        
+        return super().ready()
