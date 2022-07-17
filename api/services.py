13a3e8e9c60955f1af66a2c51a914e8a66fbd959
sviddo <snisarvlad04@gmail.com>
2022-07-17 19:56:02 +0300

    add service function to change request status to 'p'

--- api/services.py
+++ api/services.py
@@ -1,5 +1,10 @@
 from .models import Request
-from .models import UserRole, Request
+from .models import (
+    UserRole, 
+    Request,
+    RequestHistory,
+)
+from datetime import datetime
 
 
 class CustomException(Exception):
@@ -31,3 +36,21 @@ def get_request(request: Request):
     request_data['paymant_day'] = request.paymant_day
 
     return request_data
+
+
+def wait_to_change_request_status(request: Request):
+    paymant_day = request.paymant_day
+    paymant_day_year = paymant_day.year
+    paymant_day_month = paymant_day.month
+    paymant_day_day = paymant_day.day
+    diff = datetime.now() - datetime(
+        paymant_day_year,
+        paymant_day_month,
+        paymant_day_day,
+    )
+
+    if diff.total_seconds() <= 0:
+        request.status = 'p'
+        request.save()
+        RequestHistory(request=request, type_of_change='p').save()
+
