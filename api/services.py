34f6b43189bc5bff3d5f2324b5ff6dfdcef51fa4
sviddo <snisarvlad04@gmail.com>
2022-07-17 22:13:07 +0300

    catch error caused by serice function

--- api/services.py
+++ api/services.py
@@ -40,18 +40,21 @@ def get_request(request: Request):
 
 
 def wait_to_change_request_status(request: Request):
-    paymant_day = request.paymant_day
-    paymant_day_year = paymant_day.year
-    paymant_day_month = paymant_day.month
-    paymant_day_day = paymant_day.day
-    diff = datetime.now() - datetime(
-        paymant_day_year,
-        paymant_day_month,
-        paymant_day_day,
-    )
+    try:
+        paymant_day = request.paymant_day
+        paymant_day_year = paymant_day.year
+        paymant_day_month = paymant_day.month
+        paymant_day_day = paymant_day.day
+        diff = datetime.now() - datetime(
+            paymant_day_year,
+            paymant_day_month,
+            paymant_day_day,
+        )
 
-    if diff.total_seconds() >= 0:
-        request.status = 'p'
-        request.save()
-        RequestHistory(request=request, type_of_change='p').save()
+        if diff.total_seconds() >= 0:
+            request.status = 'p'
+            request.save()
+            RequestHistory(request=request, type_of_change='p').save()
+    except AttributeError:
+        pass
 
