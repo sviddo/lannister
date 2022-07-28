bc7f5828dfeb8e75e17fac572af24141bc1cc820
sviddo <snisarvla04@gmail.com>
2022-07-28 06:33:05 +0300

    add delete reviewer role tests:
    - if user id isn't valid reviewer role can't be deleted
    - if user id is proper and user is reviewer then he gets worker again
    - if user is worker reviewer role can't be deleted
    - also change names of some test functions and respectively names of dependencies
    - move service.py file into subdirectory

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,8 +1,7 @@
 import json
 import requests
 import pytest
-
-from conftest import return_users_to_add, return_workers
+from services import return_users_to_add, return_workers
 
 url = "http://127.0.0.1:8000"
 
@@ -128,17 +127,17 @@ class TestHandleReviewerRole:
                              ["abjdsbcjdcjdcjdjjdkj",
                               "1234567891234",
                               "add-user-4"])
-    def test_invalid_data(self, test):
+    def test_add_invalid_data(self, test):
         entire_url = url + f"/api/reviewer_role/{test}"
         single_user = requests.patch(url=entire_url)
         
         assert single_user.status_code == 400
         
         
-    @pytest.mark.dependency(name="TestHandleReviewerRole::test_valid_data", depends=["TestAddSingleUser::test_valid_data"])
+    @pytest.mark.dependency(name="TestHandleReviewerRole::test_add_valid_data", depends=["TestAddSingleUser::test_valid_data"])
     @pytest.mark.parametrize("test", 
                              return_workers())
-    def test_valid_data(self, test):
+    def test_add_valid_data(self, test):
         requests.post(url=url + "/api/add_user", json=test)  # add worker in database to make him reviewer
         entire_url = url + f"/api/reviewer_role/{test['service_id']}"
         single_user = requests.patch(url=entire_url)
@@ -147,11 +146,43 @@ class TestHandleReviewerRole:
             single_user.status_code == 400 and single_user.json() == ["User is already reviewer!"]
         
         
-    @pytest.mark.dependency(depends=["TestHandleReviewerRole::test_valid_data"])
+    @pytest.mark.dependency(depends=["TestHandleReviewerRole::test_add_valid_data"])
     @pytest.mark.parametrize("test", 
                              return_workers())
-    def test_same_valid_data(self, test):
+    def test_add_same_valid_data(self, test):
         entire_url = url + f"/api/reviewer_role/{test['service_id']}"
         single_user = requests.patch(url=entire_url)
         
         assert single_user.status_code == 400 and single_user.json() == ["User is already reviewer!"]
+        
+        
+    @pytest.mark.parametrize("test",
+                             ["abjdsbcjdcjdcjdjjdkj",
+                              "1234567891234",
+                              "add-user-4"])
+    def test_delete_invalid_data(self, test):
+        entire_url = url + f"/api/reviewer_role/{test}"
+        single_user = requests.delete(url=entire_url)
+        
+        assert single_user.status_code == 400
+        
+        
+    @pytest.mark.dependency(name="TestHandleReviewerRole::test_delete_valid_data", 
+                            depends=["TestHandleReviewerRole::test_add_valid_data"])
+    @pytest.mark.parametrize("test", 
+                             return_workers())
+    def test_delete_valid_data(self, test):
+        entire_url = url + f"/api/reviewer_role/{test['service_id']}"
+        single_user = requests.delete(url=entire_url)
+        
+        assert single_user.status_code == 200
+        
+        
+    @pytest.mark.dependency(depends=["TestHandleReviewerRole::test_delete_valid_data"])
+    @pytest.mark.parametrize("test", 
+                             return_workers())
+    def test_delete_same_valid_data(self, test):
+        entire_url = url + f"/api/reviewer_role/{test['service_id']}"
+        single_user = requests.delete(url=entire_url)
+        
+        assert single_user.status_code == 400 and single_user.json() == ["User is not reviewer!"]
